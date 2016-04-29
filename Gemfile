779692170ddecfd313d30e9e59d0eb7fff65fc32
Jakub Steiner <jimmac@gmail.com>
2016-04-29 20:18:22 +0200

    placeholder data.
    - to test deployment

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman" #, github: "middleman/middleman", branch: "v3-stable"
+#gem "middleman-blog"
+gem "middleman-livereload" #3.1.0
+#gem "nokogiri" #1.6.0 (xml html smarts)
+#gem "redcarpet"
+gem "haml" #4.0.4
+gem "sass" #3.2.12
+
+# For feed.xml.builder
+# gem "builder"# , "~> 3.0"
+
+# Syntax highlighting
+# gem "middleman-syntax"
+
+#deployment
+gem "middleman-deploy"
+#gem "middleman-gh-pages"
+
+#bootstrap
+#gem "bootstrap-sass" # "3.0.0"
---
+++ config.rb
@@ -0,0 +1,54 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
