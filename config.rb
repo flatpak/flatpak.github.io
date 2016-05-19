8afdfebc975a4c9d8863786c58ebf10643c26b8a
Jakub Steiner <jimmac@gmail.com>
2016-05-19 17:04:37 +0200

    deploy to master

--- config.rb
+++ config.rb
@@ -50,7 +50,7 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
+  deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
