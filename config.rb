2de21e3a8385ef35f7ea1993922a2d83aa377dde
Jakub Steiner <jimmac@gmail.com>
2017-04-11 12:48:40 +0200

    unbreak middleman-deploy
    - new syntax for config of middleman-deploy

--- config.rb
+++ config.rb
@@ -56,12 +56,8 @@ activate :relative_assets
 set :relative_links, true
 
 activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.method = :git
   deploy.branch   = 'master' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
 #activate :redcarpet
