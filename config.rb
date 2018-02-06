846647e74d766648c7f262bd560b254652ff347b
Jakub Steiner <jimmac@gmail.com>
2018-02-06 12:20:45 +0100

    update GA id
    turns out I was using racefv.eu id. yay me.

--- config.rb
+++ config.rb
@@ -54,7 +54,7 @@ set :markdown_engine, :redcarpet
 set :markdown, :tables => true #, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true
 
 activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-68811788-1' # Replace with your property ID.
+  ga.tracking_id = 'UA-77917849-1' # Replace with your property ID.
 end
 
 #activate :asset_hash
